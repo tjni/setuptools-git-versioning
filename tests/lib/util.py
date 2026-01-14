761b840460f55f9e7b085b6dae1b2d2b0e849c05
Martynov Maxim <martinov_m_s_@mail.ru>
2026-01-15 02:19:49 +0300

    Fix tests

--- tests/lib/util.py
+++ tests/lib/util.py
@@ -322,10 +322,6 @@ def get_version_module(cwd: str | os.PathLike, args: list[str] | None = None, **
 def get_version_script(cwd: str | os.PathLike, args: list[str] | None = None, **kwargs) -> str:
     return execute(
         cwd,
-        sys.executable,
-        "-m",
-        "coverage",
-        "run",
         "setuptools-git-versioning",
         *(args or []),
         "-vv",
